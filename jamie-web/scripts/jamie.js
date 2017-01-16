5126ba97eaecd337e73fd45642013c1429c9c119
Raiko Triebler <r.triebler@pinguin.ag>
2017-01-16 23:56:08 +0100

    chnage dapi url

--- jamie-web/scripts/jamie.js
+++ jamie-web/scripts/jamie.js
@@ -1,7 +1,7 @@
 (function () {
     var app = angular
         .module('jamieApp', ['xeditable', 'weatherDirective', 'restaurantDirective', 'developerDirective', 'likingDirective', 'dailyPreferenceDirective'])
-        .constant('SERVER_URL', 'http://dominion-of-the-bytes.com/jamie');
+        .constant('SERVER_URL', 'http://www.dominion-of-the-bytes.com/jamie-api');
 
     app.controller('MainController', ['$scope', '$http', '$log', 'SERVER_URL', function ($scope, $http, $log, SERVER_URL) {
         $scope.developers = [];
